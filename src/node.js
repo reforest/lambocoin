67f8184b28e4ac0804dc3f3276be0e46da65ec76
Andy Chen <amazingandyyy@gmail.com>
2018-04-06 23:22:50 -0700

    [add]wallet

--- src/node.js
+++ src/node.js
@@ -1,11 +1,11 @@
 import lotion from 'lotion';
 import coins from 'coins';
 
-const PORT = 3000;
+const PORT = process.argv[2] || 3000;
 const app = lotion({ initialState: {} });
 
 app.use(coins({
-  name: 'kittycoin',
+  name: 'basecoin',
   initialBalances: {
     andy: 100000
   }
@@ -14,5 +14,5 @@ app.use(coins({
 // console.log(Object.keys(app));
 
 app.listen(PORT).then(() => {
-  console.log(`Coin availabled on PORT ${PORT}`);
+  console.log(`\x1b[33m[chain]\t Coin availabled on PORT ${PORT}\x1b[0m`);
 });
