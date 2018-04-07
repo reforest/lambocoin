99d033b559f3e6360ef820a5518bd1f2d861fc12
Andy Chen <amazingandyyy@gmail.com>
2018-04-07 14:56:29 -0700

    rebrand to lambocoin

--- src/app.js
+++ src/app.js
@@ -7,5 +7,5 @@ const app = lotion({ initialState: {} });
 app.use(handler);
 
 app.listen(PORT).then(() => {
-  console.log(`\x1b[33m[chain]\t Basecoin availabled on PORT ${PORT}\x1b[0m`);
+  console.log(`\x1b[33m[chain]\t lambocoin availabled on PORT ${PORT}\x1b[0m`);
 });
