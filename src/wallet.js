4400c9e65ce8ed1f46121fbf9bcdb3aa8006563a
Ryan Cheung <cheung31@gmail.com>
2018-04-06 23:46:28 -0700

    Install https://github.com/keppel/coins/tree/judd/wallet-and-docs branch for coins dep

--- src/wallet.js
+++ src/wallet.js
@@ -1,20 +1,17 @@
-import lotion from 'lotion';
-import coins from 'coins';
+let lotion = require('lotion');
+let coins = require('coins');
+let client = await lotion.connect(APP_GCI);
+let { randomBytes } = require('crypto');
 
-let client;
-const APP_GCI = '';
-lotion.connect().then((c)=>{
-  client = c;
-});
-
-let wallet = coins.wallet(client)
+let priv = randomBytes(32);
+let wallet = coins.wallet(priv, client);
 
 // wallet methods:
-let address = wallet.getAddress()
-console.log(address) // 'OGccsuLV2xuoDau1XRc6hc7uO24'
+let address = wallet.getAddress();
+console.log(address); // 'OGccsuLV2xuoDau1XRc6hc7uO24'
 
-// let balance = await wallet.getBalance()
-// console.log(balance) // 20
+let balance = await wallet.getBalance();
+console.log(balance); // 20
 
-// let result = await wallet.send('04oDVBPIYP8h5V1eC1PSc/JU6Vo', 5)
-// console.log(result) // { height: 42 }
+let result = await wallet.send('04oDVBPIYP8h5V1eC1PSc5JU6Vo', 5)
+console.log(result); // { height: 42 }
