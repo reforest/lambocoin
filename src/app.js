68310551c1c984bdea098cc8cbc0e139a49d23d1
Andy Chen <amazingandyyy@gmail.com>
2018-04-07 17:01:45 -0700

    deploy

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ class App {
     console.log('\x1b[33m[lambocoin]\t initialized...\x1b[0m');
   }
   listen(_port) {
-    this.app.use(handler);
-    this.app.listen(_port).then(() => {
-      console.log(`\x1b[32m[lambocoin]\t available on PORT ${_port}\x1b[0m`);
-    });
+    new Promise((resolve, reject)=>{
+      this.app.use(handler);
+      return this.app.listen(_port).then(() => {
+        console.log(`\x1b[32m[lambocoin]\t available on PORT ${_port}\x1b[0m`);
+        resolve(true)
+      });
+    })
   }
 }
 
